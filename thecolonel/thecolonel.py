143eb6530077581615ac0c0233cdd02fc7875b0d
diegoasua <baxua@hotmail.es>
2020-06-15 19:04:21 +0200

    First commit with some common functions

--- thecolonel/thecolonel.py
+++ thecolonel/thecolonel.py
@@ -1,1 +1,35 @@
 """Main module."""
+from queue import Empty
+from PyQt5.QtCore import QTimer, Qt
+from PyQt5.QtWidgets import (
+    QWidget,
+    QRadioButton,
+    QHBoxLayout,
+    QVBoxLayout,
+    QMainWindow,
+    QDockWidget,
+    QTabWidget
+)
+
+
+def get_last_parameters(parameter_queue, timeout=0.001):
+    params = None
+    while True:
+        try:
+            params = parameter_queue.get(timeout=timeout)
+        except Empty:
+            break
+    return params
+
+
+class DockedWidget(QDockWidget):
+    def __init__(self, widget=None, layout=None, title=""):
+        super().__init__()
+        if widget is not None:
+            self.setWidget(widget)
+        else:
+            self.setWidget(QWidget())
+            self.widget().setLayout(layout)
+        if title != "":
+            self.setWindowTitle(title)
+
